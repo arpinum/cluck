a217b43f0410a1697e0581346a5af60a01f14f2c
Michael Borde <michael.borde@ubimail.fr>
2015-08-01 12:00:03 +0200

    Files refactoring

--- build/mocha.js
+++ build/mocha.js
@@ -1,22 +1,23 @@
 'use strict';
 
 var _ = require('lodash');
-var glob = require('glob');
 var Mocha = require('mocha');
 var Bluebird = require('bluebird');
 
-module.exports = function () {
-  var desktopSpecs = glob.sync('lib/**/*.spec.js');
-  var mocha = new Mocha({reporter: 'spec'});
-  _.forEach(desktopSpecs, function (spec) {
-    mocha.addFile(spec);
-  });
-  return new Bluebird(function (resolve, reject) {
-    mocha.run(function (failures) {
-      if (failures) {
-        reject();
-      }
-      resolve();
+module.exports = function (drunk) {
+  return drunk.files.promiseGlobbing('lib/**/*.spec.js')
+    .then(function (files) {
+      var mocha = new Mocha({reporter: 'spec'});
+      _.forEach(files, function (spec) {
+        mocha.addFile(spec);
+      });
+      return new Bluebird(function (resolve, reject) {
+        mocha.run(function (failures) {
+          if (failures) {
+            reject();
+          }
+          resolve();
+        });
+      });
     });
-  });
 };
