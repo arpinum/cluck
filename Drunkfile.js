7182c515c78739ec6405a85538a08bed64c9bb16
Michael Borde <michael.borde@ubimail.fr>
2015-08-01 10:47:16 +0200

    Shell refactoring

--- Drunkfile.js
+++ Drunkfile.js
@@ -1,15 +1,13 @@
 'use strict';
 
 var _ = require('lodash');
-var path = require('path');
-var spawn = require('cross-spawn');
 var glob = require('glob');
 var Mocha = require('mocha');
 var Bluebird = require('bluebird');
 
 module.exports = function (drunk) {
   drunk.register('jshint', function () {
-    executeBin('jshint', ['.']);
+    drunk.shell.promiseLocalBinExecution('jshint', ['.']);
   });
 
   drunk.register('mocha', function () {
@@ -27,22 +25,4 @@ module.exports = function (drunk) {
       });
     });
   });
-
-  function executeBin(binary, args, options) {
-    var shellOptions = _.merge(defaultShellOptions(), options);
-    var binaryPath = resolveExecutable(binary);
-    var execution = spawn.sync.call(null, binaryPath, args, shellOptions);
-    if (execution.status !== 0) {
-      var error = execution.error || new Error('Task failed');
-      console.error(error);
-    }
-  }
-
-  function resolveExecutable(name) {
-    return path.resolve('node_modules', '.bin', name);
-  }
-
-  function defaultShellOptions() {
-    return {stdio: 'inherit', encoding: 'UTF-8'};
-  }
 };
