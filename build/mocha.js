595c43ec30d5a7540816dd979fc0fac1d08b39e5
Michael Borde <michael.borde@ubimail.fr>
2015-08-21 18:27:20 +0200

    Stats options

--- build/mocha.js
+++ build/mocha.js
@@ -1,23 +1,8 @@
 'use strict';
 
-var _ = require('lodash');
-var Mocha = require('mocha');
-var Bluebird = require('bluebird');
-
 module.exports = function (cluck) {
-  return cluck.files.glob('lib/**/*.spec.js')
-    .then(function (files) {
-      var mocha = new Mocha({reporter: 'spec'});
-      _.forEach(files, function (spec) {
-        mocha.addFile(spec);
-      });
-      return new Bluebird(function (resolve, reject) {
-        mocha.run(function (failures) {
-          if (failures) {
-            reject();
-          }
-          resolve();
-        });
-      });
-    });
+  return cluck.shell.execute(
+    'mocha',
+    ['--colors', '--reporter', 'spec', '--recursive', 'lib'],
+    {resolveLocalBin: true});
 };
